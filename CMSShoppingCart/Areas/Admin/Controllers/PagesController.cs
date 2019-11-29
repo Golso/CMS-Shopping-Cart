e92b95c84725c6418df63395e40e4d949d8724a2
Golso <matq666@gmail.com>
2019-11-29 13:50:29 +0100

    Add page and edit page

---
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,117 @@
+﻿using CMSShoppingCart.Models.Data;
+using CMSShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMSShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of PageVM
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                //Init the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return view with list
+            return View(pagesList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //check model state
+            if(! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //declare the slug
+                string slug;
+
+                //init pageDTO
+                PageDTO dto = new PageDTO();
+
+                //DTO title
+                dto.Title = model.Title;
+
+                //check for and set slug if need be
+                if(string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists.");
+                    return View(model);
+                }
+
+                //DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+                dto.Sorting = 100;
+
+                //save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //set TempData message
+            TempData["SM"] = "You have added a new page.";
+
+            //redirect
+            return RedirectToAction("AddPage");
+        }
+
+        // GET: Admin/Pages/EditPage/id
+        public ActionResult EditPage(int id)
+        {
+            //declare pageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                //get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                    
+                //confirm page exists
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                //init pageVM
+                model = new PageVM(dto);
+            }
+            //return view with model
+            return View(model);
+        }
+    }
+}
