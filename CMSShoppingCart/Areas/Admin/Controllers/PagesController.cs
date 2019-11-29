9a0def24c240a1a5e4de717809ac7a1716671809
Golso <matq666@gmail.com>
2019-11-29 17:26:01 +0100

    Admin pages finished.

--- CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -90,6 +90,7 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/EditPage/id
+        [HttpGet]
         public ActionResult EditPage(int id)
         {
             //declare pageVM
@@ -111,7 +112,181 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
                 model = new PageVM(dto);
             }
             //return view with model
+            return View(model
+);
+        }
+
+        // POST: Admin/Pages/EditPage/id
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            //check model state
+            if (! ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //get page id
+                int id = model.Id;
+
+                //init slug
+                string slug="home";
+
+                //get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                //DTO the title
+                dto.Title = model.Title;
+
+                //check for slug and set it if need be
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                //make sure if title and slug are unique
+                if(db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) || db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists.");
+                    return View(model);
+                }
+
+                //DTO the rest
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+
+                //save the DTO
+                db.SaveChanges();
+            }
+
+            //set TempData message
+            TempData["SM"] = "You have edited the page.";
+
+            //redirect
+            return RedirectToAction("EditPage");
+        }
+
+        // GET: Admin/Pages/PageDetails/id
+        public ActionResult PageDetails(int id)
+        {
+            //declare PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                //get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                //confirm page exists
+                if(dto==null)
+                {
+                    return Content("The page does not exists");
+                }
+
+                //init the PageVM
+                model = new PageVM(dto);
+            }
+            //return the view with the model
             return View(model);
+        }
+
+        // GET: Admin/Pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                //get the page
+                PageDTO dto = db.Pages.Find(id);
+
+                //remove the page
+                db.Pages.Remove(dto);
+
+                //save
+                db.SaveChanges();
+            }
+
+            //redirect
+            return RedirectToAction("Index");
+
+        }
+
+        // POST: Admin/Pages/ReorderPages
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //set initial count
+                int count = 1;
+
+                //declare pageDTO
+                PageDTO dto;
+
+                //set sorting for each page
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+
+        }
+
+        // GET: Admin/Pages/EditSidebar
+        [HttpGet]
+        public ActionResult EditSidebar()
+        {
+            //declare model
+            SidebarVM model;
+
+            using (Db db = new Db())
+            {
+                //get the DTO
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                //init model
+                model = new SidebarVM(dto);
+            }
+
+            //return view with model
+            return View(model);
+        }
+
+        // POST: Admin/Pages/EditSidebar
+        [HttpPost]
+        public ActionResult EditSidebar(SidebarVM model)
+        {
+            using (Db db = new Db())
+            {
+                //get the DTO
+                SidebarDTO dto = db.Sidebar.Find(1);
+
+                //DTO the body
+                dto.Body = model.Body;
+
+                //Save
+                db.SaveChanges();
+            }
+
+            //set TempData message
+            TempData["SM"] = "You have edited the sidebar.";
+
+            //redirect
+            return RedirectToAction("EditSidebar");
         }
     }
 }
